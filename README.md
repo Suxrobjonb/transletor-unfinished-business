29f7f89ef1bf8debf76f7b68f3ec95500c133d0f
Suxrobjonb <suxrobjonb6@gmail.com>
2022-09-25 21:05:10 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# transletor-unfinished-business
---
+++ js/app.js
@@ -0,0 +1,61 @@
+let elAlert = alert("bu dastur faqat:mashina, uy, soat, yil, oila, aftobus, salom haftakunlari. sozlarini tarjima qiladi javob consol logda");
+alert("aytgancha bunda inliz va o'zbek tillari mavjud.")
+let elPrompt = prompt("kirirting")
+
+switch(elPrompt) {
+    case "dushanba":
+    console.log("monday");
+    break;
+    
+    case "seshanba":
+    console.log("tuesday");
+    break;
+    
+    case "chorshanba":
+    console.log("wensday");
+    break;
+    
+    case "payshanba":
+    console.log("thursday");
+    break;
+    
+    case "juma":
+    console.log("fiday");
+    break;
+    
+    case "shanba":
+    console.log("saturday");
+    break;
+    
+    case "yakshanba":
+    console.log("sunday");
+    break;
+    
+    case "mashina":
+    console.log("car");
+    break
+    
+    case "uy":
+    console.log("home");
+    break
+
+    case "soat":
+    console.log("clock");
+    break
+
+    case "yil":
+    console.log("years");
+    break
+
+    case "oila":
+    console.log("famliy");
+    break
+
+    case "aftobus":
+    console.log("bus");
+    break
+
+    case "salom":
+    console.log("hello");
+    break
+}
